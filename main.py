1fe98d37b41e5e16cf1fb1136a345eeca17c63e6
aronnicksnts <aronnicksnts@gmail.com>
2023-04-09 08:13:20 +0800

    added multiprocessing

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 import get_data
-import pandas as pd
-import numpy as np
 import cv2
 import image_manipulation
+from multiprocessing import Pool
+from p_tqdm import p_map
+import itertools
 
+# All editable variables
 image_paths = get_data.MURA_DATASET()
-
+new_file_path = "MURA-v1.1/augmented/train"
+all_image_paths = image_paths.get_image_paths(train=True).to_numpy()
+# horizontal flip augmentation
+augment_hflip = True
+# vertical flip augmentation
+augment_vflip = True
+# 0 for no rotation
+max_rotation = 0
+# max threads to be used
+num_processes = 8
 
 # Applies various data_cleaning methods chosen from data_cleaning.py
 def apply_data_cleaning(image):
@@ -17,19 +28,20 @@ def apply_data_cleaning(image):
 # Iterate on image paths and apply data cleaning processes
 # Saves images on new_file_path and gets images from image_paths
 # image_paths should be a tuple containing (integer, string of path, label)
-def process_images(new_file_path, image_paths):
-    for image_path in image_paths:
-        image = cv2.imread(image_path[1])
+def process_images(new_file_path, image_path):
 
-        image = apply_data_cleaning(image)
-        new_train_file_path = f"{new_file_path}/{image_path[1].replace('/','_').replace('.png', '')}"
+    image = cv2.imread(image_path[0])
+    image = apply_data_cleaning(image)
+    new_train_file_path = f"{new_file_path}/{image_path[0].replace('/','-').replace('.png', '')}"
 
-        #augments the data
-        new_images = image_manipulation.augment_data(image, hflip= True)
-        for index, img in enumerate(new_images):
-            cv2.imwrite(f"{new_train_file_path}_{index}.png", img)
-        print(new_train_file_path)
-        break
+    #augments the data
+    new_images = image_manipulation.augment_data(image, hflip= augment_hflip, vflip=augment_vflip, max_rotation=max_rotation)
+    for index, img in enumerate(new_images):
+        cv2.imwrite(f"{new_train_file_path}_{index}.png", img)
+    return
 
-process_images("MURA-v1.1/augmented/train", image_paths.get_image_paths(train=True).itertuples())
 
+if __name__ == "__main__":
+    print("Processing Images")
+    with Pool(num_processes) as p:
+        p_map(process_images, itertools.repeat(new_file_path, len(all_image_paths)), all_image_paths)

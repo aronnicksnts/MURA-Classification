95175ca64071ff79e6ebf07af362750ed6579dac
Aron Nick Santos <aronnicksnts@gmail.com>
2023-04-17 06:30:47 +0800

    Separated Dataset and Preprocessing

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from os import listdir
 import numpy as np
 import glob
 from math import ceil, floor
+from os import path, mkdir
 
 # All editable variables
 image_paths = MURA.MURA_DATASET()
@@ -22,12 +23,21 @@ validation_set = 0.05
 testing_set = 0.55
 
 
-# horizontal flip augmentation
-augment_hflip = False
-# vertical flip augmentation
-augment_vflip = False
-# 0 for no rotation
-max_rotation = 0
+# Augmentation parameters
+training_augmentation = {'augment_hflip': False,
+ 'augment_vflip': False,
+ 'max_rotation': 0}
+
+testing_augmentation = {'augment_hflip': False,
+ 'augment_vflip': False,
+ 'max_rotation': 0}
+
+validation_augmentation = {'augment_hflip': False,
+ 'augment_vflip': False,
+ 'max_rotation': 0}
+
+augmentations = [training_augmentation, validation_augmentation, testing_augmentation]
+
 # max threads to be used
 num_processes = 8
 # num of epochs
@@ -47,24 +57,31 @@ def apply_data_cleaning(image):
 # Iterate on image paths and apply data cleaning processes
 # Saves images on new_file_path and gets images from image_paths
 # image_paths should be a tuple containing (integer, string of path, label)
-def process_images(new_file_path, image_path):
+def process_image(new_file_path, image_path, augmentation_data):
 
-    image = cv2.imread(image_path[0])
+    image = cv2.imread(image_path)
     image = apply_data_cleaning(image)
-    new_train_file_path = f"{new_file_path}/{image_path[0].replace('/','-').replace('.png', '')}"
+    new_train_file_path = f"{new_file_path}/{image_path.replace('/','-').replace('.png', '')}"
 
     #augments the data
-    new_images = image_manipulation.augment_data(image, hflip= augment_hflip, vflip=augment_vflip, max_rotation=max_rotation)
+    new_images = image_manipulation.augment_data(image, hflip= augmentation_data['augment_hflip'], 
+                                                                                 vflip=augmentation_data['augment_vflip'],
+                                                                                 max_rotation=augmentation_data['max_rotation'])
     for index, img in enumerate(new_images):
         cv2.imwrite(f"{new_train_file_path}_{index}.png", img) 
     return
 
+def create_dir(parent_dir, new_dir_name):
+    if path.isdir(f'{parent_dir}/{new_dir_name}'):
+        return
+    else:
+        mkdir(f'{parent_dir}/{new_dir_name}')
 
 if __name__ == "__main__":
-    print("Shuffling Dataset")
+    print("Shuffling Dataset\n")
     np.random.shuffle(all_image_paths)
 
-    print("Splitting Dataset")
+    print("Splitting Dataset\n")
     total_images = len(all_image_paths)
     positives = []
     negatives = []
@@ -77,6 +94,7 @@ if __name__ == "__main__":
     training = []
     validation = []
     testing = []
+    
     for i in range(ceil(total_images*training_set)):
         training.append(negatives.pop())
     for i in range(floor(total_images*validation_set)):
@@ -87,11 +105,20 @@ if __name__ == "__main__":
         testing.append(negatives.pop())
 
     np.random.shuffle(testing)
+    datasets = [training, validation, testing]
+    dir_names = ['training', 'validation', 'testing']
+    for i in range(3):
+        create_dir(new_file_path, dir_names[i])
+        print(f"Processing {dir_names[i]} images\n")
+        p_map(process_image, itertools.repeat(f'{new_file_path}/{dir_names[i]}', len(datasets[i])), 
+                                               datasets[i], 
+                                               itertools.repeat(augmentations[i], len(datasets[i])))
+
 
     # PreProcesses the images
     # print("Processing Images")
     # with Pool(num_processes) as p:
-    #     p_map(process_images, itertools.repeat(new_file_path, len(all_image_paths)), all_image_paths)
+    #     p_map(process_image, itertools.repeat(new_file_path, len(all_image_paths)), all_image_paths)
 
     # Puts all images in a single array and converts them into a numpy array
     # all_images = []

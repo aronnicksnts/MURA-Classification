9dc9ecddf0d87e58f0715ff02389e927c31ed501
Aron Nick Santos <aronnicksnts@gmail.com>
2023-05-25 03:07:02 +0800

    added modified_dataset

--- MURA.py
+++ MURA.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import numpy as np
+import os
+import glob
 
 class MURA_DATASET:
     def __init__(self):
@@ -15,8 +17,21 @@ class MURA_DATASET:
         self.valid_image_paths.loc[self.valid_image_paths[0].str.contains("positive"), 'label'] = 1
         self.valid_image_paths.loc[self.valid_image_paths[0].str.contains("negative"), 'label'] = 0
 
+        # Get all image filepath and labels
+        self.modified_dataset_path = f"MURA-v1.1/modified_dataset"
+
+        # Get every images in the folder and label each of the images either 1 or 0
+        self.modified_dataset_paths = glob.glob(f"{self.modified_dataset_path}/*.png")
+        self.modified_dataset_paths = pd.DataFrame(self.modified_dataset_paths)
+        for i in range(len(self.modified_dataset_paths)):
+            self.modified_dataset_paths.loc[i, 'label'] = 1 if "positive" in self.modified_dataset_paths.loc[i, 0] else 0
+
+
     def get_image_paths(self, train = False):
         return self.train_image_paths if train else self.valid_image_paths
     
     def get_combined_image_paths(self):
         return pd.concat([self.train_image_paths, self.valid_image_paths], ignore_index=True)
+    
+    def get_modified_dataset_paths(self):
+        return self.modified_dataset_paths
